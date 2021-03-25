41baf710dea35cc234e05c18a24e41b659d2262b
Harshvardhan Deshmukh <47944639+Harshdeshmukh@users.noreply.github.com>
2021-03-25 13:29:15 +0530

    First commit

---
+++ tea1/index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<!-- Created By CodingNepal -->
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>तुलसी चहा</title>
+    <link rel="stylesheet" href="style1.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"/>
+    
+
+</head>
+<body>
+    <div class="scroll-up-btn">
+        <i class="fas fa-angle-up"></i>
+    </div>
+    <nav class="navbar py-0" style="background-color: ;">
+
+        <div class="max-width">
+            <div class="logo"><img src="images/logo.png" width="70" height="70" /></div>
+            <ul class="menu">
+                <li ><a href="index.php" class="menu-btn">Home</a></li>
+                <li><a href="about.php" class="menu-btn">About</a></li>
+                <li><a href="products.php" class="menu-btn">Products</a></li>
+                <li><a href="branch.php" class="menu-btn">Branch</a></li>
+                <li><a href="gallary.php" class="menu-btn">Gallary</a></li>
+                <li><a href="#contact.php" class="menu-btn">Contact</a></li>
+            </ul>
+            <div class="menu-btn">
+                <i class="fas fa-bars"></i>
+            </div>
+        </div>
+    </nav>
+
+    <!-- home section start -->
+    <section class="home" id="home">
+      <div class="max-width">
+          <div class="row">
+            <div class="home-content">
+                <div class="text-1">पंढरपूरचा सुप्रसिद्ध चहा</div>
+                <div class="text-2">तुलसी चहा</div>
+                <a href="#">Read More..</a>
+            </div>
+          </div>
+      </div>
+    </section>
+    <!-- about section start -->
+<!-- Please Subscribe My Youtube Channel
+https://www.youtube.com/channel/UCQM5ye1xR4vJgtga0ryud2Q/
+-->
+
+  <!-- about section start -->
+    <section class="about" id="about">
+        <div class="max-width">
+            
+            <div class="about-content">
+                <div class="column left">
+                    <img src="images/logo.png" alt="">
+                </div>
+                <div class="column right">
+                    
+                    <div class="text" style="color: green;"><div style="text-decoration: underline;font-size: 20px;">तुलसी चहा<br></div><span class="typing-2"></span>
+                  <div style="color: black"> पंढरपूरचा सुप्रसिद्ध चहा</div>
+                </div>
+                    <p>
+                        उत्तम चव, स्वच्छता, सुटसुटीत मांडणी या आणि अशा अनेक वैशिष्ट्यांनी परिपूर्ण तुलसी चहाची स्थापना २०१८ ला झाली. लोकांच्या पसंतीस उतरत हरमन चहाच्या चवीनं अगदी अल्पावधीतच ग्राहकांना आपलंसं केलं. ग्राहकांना केव्हाही आनंद देणारी कडक, उत्साहवर्धक, जिभेवर रेंगाळणारी सर्वोत्कृष्ट चव ही हरमन चहाची खासियत. आज तुलसी चहाचा विस्तार व ख्याती सर्वदूर पसरत आहे. ग्राहकांचे समाधान व इच्छुक व्यावसायिकांना फ्रँचायझीच्या माध्यमातून कमी गुंतवणुकीत व्यवसायाची सुवर्णसंधी असे उद्दिष्ट डोळ्यासमोर ठेवत तुलसी चहा वाटचाल सुरू आहे.
+                    </p>
+                    <a href="#">Read More..</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- footer section start-->
+    <footer>
+
+        <span>Created By <a href="#">Softech Coders</a> | <span class="far fa-copyright"></span> 2020 All rights reserved.</span>
+    </footer>
+
+   
+ 
+    <script src="js/script.js"></script>
+</body>
+</html>
